c96252b8a9fffee29021d0918148b576c9738a3b
Paulo Henrique <henriq10@outlook.com>
2018-04-22 22:47:34 -0300

    Versão estavel

--- System/Database/Model.php
+++ System/Database/Model.php
@@ -9,7 +9,7 @@ namespace System\Database;
  *
  * @author Paulo Henrique Ramos Ferreira
  */
-class Model extends Connection {
+class Model {
 
     /**
      * Tabela atual da instancia que ira se trabalhar
@@ -19,23 +19,142 @@ class Model extends Connection {
     protected $table;
 
     /**
+     * Variavel que contem a instancia do banco de dados
+     *
+     * @var Connection
+     */
+    protected $db;
+
+    /**
+     * Variavel que contem os resultados da consulta para que elas possam ser dinamicas
+     *
+     * @var PDOStatement
+     */
+    private $stmt;
+
+    /**
      * Inicia o construtor da classe pai
      * 
      * @return void
      */
     public function __construct() {
-        parent::__construct();
+        $this->db = Connection::getInstance();
     }
 
-    public function create($data) {}
+    /**
+     * Cria um novo registro no banco de dados
+     *
+     * @param array $data
+     * @return Model
+     */
+    public function create($data) {
+        $this->stmt = $this->db->prepare("INSERT INTO $this->table (".implode(", ", str_replace(":", "", array_keys($data))).") VALUES (".implode(", ", array_keys($data)).")");
+        foreach ($data as $key => $value) {
+            $this->stmt->bindValue($key, $value);
+        }
+        $this->stmt->execute();
+        return $this->db->lastInsertId();
+    }
 
-    public function read($data) {}
+    /**
+     * Faz busca de registros
+     *
+     * @param array $fields
+     * @param array $values
+     * @return Model
+     */
+    public function read($fields, $values = null) {
+        if(!$values) {
+            $this->stmt = $this->db->prepare("SELECT ".implode(", ", $fields)." FROM $this->table");
+        } else {
+            $this->stmt = $this->db->prepare("SELECT ".implode(", ", $fields)." FROM $this->table WHERE ".implode(" ", $values)."");
+        }
+        $this->stmt->execute();
+        return $this;
+    }
 
-    public function update($data) {}
+    /**
+     * Atualiza algum registro
+     *
+     * @param array $fields
+     * @param array $where
+     * @return Model
+     */
+    public function update($fields, $where = null) {
+        $sql = "";
+        foreach ($fields as $key => $value) {
+            $sql .= str_replace(":", "", $key)." = $key, ";
+        }
+        $sql = substr($sql, 0, -2);
+        if(!$where) {
+            $this->stmt = $this->db->prepare("UPDATE $this->table SET ".$sql);
+        } else {
+            $this->stmt = $this->db->prepare("UPDATE $this->table SET ".$sql." WHERE ".implode(" ", $where));
+        }
+        foreach ($fields as $key => $value) {
+            $this->stmt->bindValue($key, $value);
+        }
+        $this->stmt->execute();
+        return $this;
+    }
 
-    public function delete($data) {}
+    /**
+     * Deleta algum registro
+     *
+     * @param array $data
+     * @return Model
+     */
+    public function delete($data) {
+        $this->stmt = $this->db->prepare("DELETE FROM $this->table WHERE ".implode(" ", $data)."");
+        $this->stmt->execute();
+        return $this;
+    }
 
-    public function find($id) {}
+    /**
+     * Executa query sql personalizada
+     *
+     * @param string $query
+     * @return Model
+     */
+    public function query($query) {
+        $this->stmt = $this->db->prepare($query);
+        $this->stmt->execute();
+        return $this;
+    }
 
-    public function query($query) {}
+    /**
+     * Retorna um unico conjunto de dados de uma busca
+     *
+     * @return array
+     */
+    public function get() {
+        return $this->stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Retorna um array de dados de uma busca
+     *
+     * @return array
+     */
+    public function getAll() {
+        return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Retorna o total da consulta
+     *
+     * @return int
+     */
+    public function count() {
+        return $this->stmt->fetch(\PDO::FETCH_NUM);
+    }
+
+    /**
+     * Retorna um booleano informando se tem ou não determinado registro
+     *
+     * @return boolean
+     */
+    public function has() {
+        return (boolean) $this->stmt->rowCount();
+    }
 }
